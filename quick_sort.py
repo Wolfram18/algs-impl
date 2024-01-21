f8d5513594a1f90912cc851f84be444878e4c6c9
Victoria Ruleva <56358902+Wolfram18@users.noreply.github.com>
2024-01-21 20:26:54 +0300

    Add quick sort

---
+++ quick_sort.py
@@ -0,0 +1,21 @@
+import sys
+import re
+
+def quick_sort(numbers):
+    if (len(numbers) == 1 or len(numbers) == 0):
+        return numbers
+    else:
+        pivot = numbers[int(len(numbers)/2)]
+    left = [n for n in numbers if n < pivot]
+    equal = [n for n in numbers if n == pivot]
+    right = [n for n in numbers if n > pivot]
+    return quick_sort(left) + equal + quick_sort(right)
+
+def main():
+    for line in sys.stdin: # get input strings one by one
+        str_list = line[:-1].split(sep=',') # split by ','
+        str_list = quick_sort(str_list)
+        print(','.join(map(str, str_list))) # to string
+ 
+if __name__ == "__main__":
+    main()
