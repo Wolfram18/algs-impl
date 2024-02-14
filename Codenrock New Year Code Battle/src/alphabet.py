742b7787615ffc31a71345a3ebb022f788292ccb
Victoria Ruleva <ruleva_18@mail.ru>
2024-02-14 10:24:44 +0300

    Update alphabet.py

--- Codenrock New Year Code Battle/src/alphabet.py
+++ Codenrock New Year Code Battle/src/alphabet.py
@@ -1,12 +1,13 @@
 import sys
 import string
 
-def encode():
-    alphabet = string.ascii_lowercase  # latin alphabet
-    for line in sys.stdin:  # get input strings one by one
-        line = line.replace(" ", "").rstrip('\n')
-        ind_list = [alphabet.find(x) + 1 for x in line.lower()]
-        print(','.join(map(str, ind_list)))
+alphabet = string.ascii_lowercase  # latin alphabet
+
+def encode(line):
+    line = line.replace(" ", "").rstrip('\n')
+    ind_list = [alphabet.find(x) + 1 for x in line.lower()]
+    print(','.join(map(str, ind_list)))
 
 if __name__ == '__main__':
-    encode()
+    for line in sys.stdin:  # get input strings one by one
+        encode(line)
