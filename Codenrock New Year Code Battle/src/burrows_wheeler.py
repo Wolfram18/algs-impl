3f7ea62b4fc34212bc7d47111c39f9b3b55ba1ea
Victoria Ruleva <ruleva_18@mail.ru>
2024-03-07 12:29:04 +0300

    Update burrows_wheeler.py

--- Codenrock New Year Code Battle/src/burrows_wheeler.py
+++ Codenrock New Year Code Battle/src/burrows_wheeler.py
@@ -1,6 +1,8 @@
 import sys
 
 def transformation(input):
+    if len(input) == 0:
+        return None
     cycle_list = sorted([input[i+1:]+input[:i+1] for i in range(len(input))])
     output = ""
     for s in cycle_list:
